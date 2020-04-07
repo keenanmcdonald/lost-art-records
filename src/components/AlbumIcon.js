be344fd357f104cb6f57c75944f27dcc6eb340c8
Keenan McDonald <keemcd@gmail.com>
2020-04-06 20:56:23 -0500

    minor tweaks, removed album overlay code

--- src/components/AlbumIcon.js
+++ src/components/AlbumIcon.js
@@ -11,12 +11,6 @@ function AlbumIcon(props) {
     return (
         <div onClick={() => routeToAlbumPage()} className='album-icon'>
             <img className='album-cover' src={require(`../images/${props.coverFileName}`)} alt={props.title} />
-            <div className='album-overlay'>
-                <div className='overlay-text'>
-                    <h2 className='artist'>{props.artist}</h2>
-                    <h3 className='title'>{props.title}</h3>
-                </div>
-            </div>
         </div>
     )
 }
