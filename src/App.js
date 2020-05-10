d7a6bdaf5320d46a5432b89a89addb4165c69c44
Keenan McDonald <keemcd@gmail.com>
2020-05-10 15:12:28 -0500

    json file created

--- src/App.js
+++ src/App.js
@@ -4,39 +4,68 @@ import AlbumPage from './components/AlbumPage'
 import {Route, withRouter} from 'react-router-dom'
 import './App.css';
 import ScrollToTop from './components/ScrollToTop'
-import ALBUMS from './album-data'
 
-function App(props) {
-  function toHomePage(){
-    props.history.push('/')
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      albums: [],
+      loaded: false
+    }
+
+    this.toHomePage = this.toHomePage.bind(this)
   }
 
-  return (
-    <div className="App">
-      <ScrollToTop/>
-      <header>
-        <img className='logo-title' alt='lost art records title' src={require('./images/title-logo-lightened.jpg')} onClick={() => toHomePage()}/>
-        <img className='mic-logo' alt='microphone logo' src={require('./images/mic-logo-lightened.jpg')} onClick={() => toHomePage()}/>
-      </header>
-        <Route exact path='/' render={() => <AlbumList albums={ALBUMS}/>}/>
-        <Route path='/claypigeons' render={() => <AlbumPage {...ALBUMS[0]}/>}/>
-        <Route path='/muscleshoals' render={() => <AlbumPage {...ALBUMS[1]}/>}/>
-        <Route path='/austinouthouse' render={() => <AlbumPage {...ALBUMS[2]}/>}/>
-        <Route path='/sittinbytheroad' render={() => <AlbumPage {...ALBUMS[3]}/>}/>
-        <Route path='/coldcoldworld' render={() => <AlbumPage {...ALBUMS[4]}/>}/>
-        <Route path='/ovalroom' render={() => <AlbumPage {...ALBUMS[5]}/>}/>
-        <Route path='/ducttapemessiah' render={() => <AlbumPage {...ALBUMS[6]}/>}/>
-        <Route path='/billneely' render={() => <AlbumPage {...ALBUMS[7]}/>}/>
-        <Route path='/ducttapedoc' render={() => <AlbumPage {...ALBUMS[8]}/>}/>
-      <footer>
-        <div className='contact-info'>
-          <p>1102 Live Oak Ridge, Austin, Texas 78746</p>
-          <p><a href='mailto:music@lostartrecords.com'>music@lostartrecords.com</a></p>
-          <p>512-472-9770</p>
+  toHomePage(){
+    this.props.history.push('/')
+  }
+
+  async getData() {
+    const res = await fetch("/album-data.json");
+    const albums = await res.text();
+    console.log(albums);
+    //return this.setState({albums, loaded: true});
+  }
+
+  componentDidMount() {
+    //console.log(JSON.stringify(this.state.albums))
+    this.getData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.loaded ? 
+        <div>
+          <ScrollToTop/>
+          <header>
+            <img className='logo-title' alt='lost art records title' src={require('./images/title-logo-lightened.jpg')} onClick={() => this.toHomePage()}/>
+            <img className='mic-logo' alt='microphone logo' src={require('./images/mic-logo-lightened.jpg')} onClick={() => this.toHomePage()}/>
+          </header>
+            <Route exact path='/' render={() => <AlbumList albums={this.state.albums}/>}/>
+            <Route path='/claypigeons' render={() => <AlbumPage {...this.state.albums[0]}/>}/>
+            <Route path='/muscleshoals' render={() => <AlbumPage {...this.state.albums[1]}/>}/>
+            <Route path='/austinouthouse' render={() => <AlbumPage {...this.state.albums[2]}/>}/>
+            <Route path='/sittinbytheroad' render={() => <AlbumPage {...this.state.albums[3]}/>}/>
+            <Route path='/coldcoldworld' render={() => <AlbumPage {...this.state.albums[4]}/>}/>
+            <Route path='/ovalroom' render={() => <AlbumPage {...this.state.albums[5]}/>}/>
+            <Route path='/ducttapemessiah' render={() => <AlbumPage {...this.state.albums[6]}/>}/>
+            <Route path='/billneely' render={() => <AlbumPage {...this.state.albums[7]}/>}/>
+            <Route path='/ducttapedoc' render={() => <AlbumPage {...this.state.albums[8]}/>}/>
+          <footer>
+            <div className='contact-info'>
+              <p>1102 Live Oak Ridge, Austin, Texas 78746</p>
+              <p><a href='mailto:music@lostartrecords.com'>music@lostartrecords.com</a></p>
+              <p>512-472-9770</p>
+            </div>
+          </footer>
         </div>
-      </footer>
-    </div>
-  );
+          : ''
+        }
+      </div>
+    );  
+  }
 }
 
-export default withRouter(App);
+export default withRouter(App)
