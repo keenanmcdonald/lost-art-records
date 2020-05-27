067a620f2a11071370870a7710a59e78b5f14fef
Keenan McDonald <keemcd@gmail.com>
2020-05-27 16:58:24 -0500

    redesign first commit wip

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,8 +3,10 @@ import AlbumIcon from './AlbumIcon'
 
 function AlbumList(props) {
     const albumIcons = []
+
     for (let i = 0; i < props.albums.length; i++){
         albumIcons.push(<AlbumIcon key={i} coverFileName={props.albums[i].coverFileName} title={props.albums[i].title} artist={props.albums[i].artist} pagePath={props.albums[i].pagePath}/>)
+        //albumIcons.push(<div className='album-icon standin'/>)
     }
 
     return (
